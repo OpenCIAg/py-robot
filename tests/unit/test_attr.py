f3fa181f1819e2c7848278fdbf4329e7b968d4f3
Éttore Leandro Tognoli <ettore.tognoli@ciag.org.br>
2018-04-06 07:33:24 -0300

    fix tests

--- tests/unit/test_attr.py
+++ tests/unit/test_attr.py
@@ -1,9 +1,9 @@
-from unittest import TestCase
-from robot.core import xml_engine
 import asyncio
 import re
+from unittest import TestCase
 
 from robot import CollectorFactory
+from robot.core import xml_engine
 
 
 class AttributeCollectorTest(TestCase):
@@ -21,28 +21,28 @@ class AttributeCollectorTest(TestCase):
         self.assertEqual(expected, result)
 
     def test_with_regex(self):
-        collector = self.cf.attr('div', regex=re.compile(r'user: *.+', re.IGNORECASE))
+        collector = self.cf.attr('div', regex=re.compile(r'user:[\W]*.+', re.IGNORECASE))
         html = xml_engine('<html><div>User:<p>username</p></div></html>')
-        expected = "User: username"
+        expected = r"User:[\W]*username"
         result = self.loop.run_until_complete(collector(html, None))
-        self.assertEqual(expected, result)
+        self.assertRegex(result, expected)
 
     def test_with_regex_single(self):
-        collector = self.cf.attr('div', regex=re.compile(r'user: *(.+)', re.IGNORECASE))
+        collector = self.cf.attr('div', regex=re.compile(r'user:[\W]*(.+)', re.IGNORECASE))
         html = xml_engine('<html><div>User:<p>username</p></div></html>')
         expected = "username"
         result = self.loop.run_until_complete(collector(html, None))
         self.assertEqual(expected, result)
 
     def test_with_regex_multiple(self):
-        collector = self.cf.attr('div', regex=re.compile(r'([a-z_-]+): *(.+)', re.IGNORECASE))
+        collector = self.cf.attr('div', regex=re.compile(r'([a-z_-]+):[\W]*(.+)', re.IGNORECASE))
         html = xml_engine('<html><div>User:<p>username</p></div></html>')
         expected = ("User", "username",)
         result = self.loop.run_until_complete(collector(html, None))
         self.assertEqual(expected, result)
 
     def test_with_regex_dict(self):
-        collector = self.cf.attr('div', regex=re.compile(r'(?P<key>[a-z_-]+): *(?P<value>.+)', re.IGNORECASE))
+        collector = self.cf.attr('div', regex=re.compile(r'(?P<key>[a-z_-]+):[\W]*(?P<value>.+)', re.IGNORECASE))
         html = xml_engine('<html><div>User:<p>username</p></div></html>')
         expected = dict(key="User", value="username")
         result = self.loop.run_until_complete(collector(html, None))
@@ -50,15 +50,18 @@ class AttributeCollectorTest(TestCase):
 
     def test_with_regex_filter(self):
         collector = self.cf.attr('tr', regex_filter=re.compile(r'user:', re.IGNORECASE),
-                                 regex=re.compile(r'user: *(.*)'))
+                                 regex=re.compile(r'user:[\W]*(.*)'))
         html = xml_engine('<table><tr><td>user:</td><td>username</td></tr><tr><td>age:</td><td>24</td></tr></table>')
         expected = 'username'
         result = self.loop.run_until_complete(collector(html, None))
         self.assertEqual(expected, result)
 
     def test_with_regex_filter_not_match(self):
-        collector = self.cf.attr('tr', regex_filter=re.compile(r'otherfield:', re.IGNORECASE),
-                                 regex=re.compile(r'otherfield: *(.*)'))
+        collector = self.cf.attr(
+            'tr',
+            regex_filter=re.compile(r'otherfield:', re.IGNORECASE),
+            regex=re.compile(r'otherfield:[\W]*(.*)')
+        )
         html = xml_engine('<table><tr><td>user:</td><td>username</td></tr><tr><td>age:</td><td>24</td></tr></table>')
         expected = None
         result = self.loop.run_until_complete(collector(html, None))
