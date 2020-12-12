7d35e4c3fbbe213c23533860e4677dae53fe4697
Ettore Leandro Tognoli <ettoreleandrotognoli@gmail.com>
2020-12-12 02:40:21 +0100

    const test

---
+++ src/test/python/unit/collector/test_const.py
@@ -0,0 +1,12 @@
+from aiounittest import AsyncTestCase
+
+from robot.collector.shortcut import *
+
+
+class ConstCollectorTest(AsyncTestCase):
+
+    async def test_const(self):
+        any_value = 'any value'
+        collector = const(any_value)
+        result = await collector(None, None)
+        self.assertEqual(result, any_value)
