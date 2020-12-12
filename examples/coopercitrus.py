fb28abd25eca499f5ef0bf5db2a3d75c3637d8c3
Ettore Leandro Tognoli <ettoreleandrotognoli@gmail.com>
2020-12-13 00:18:55 +0100

    rewrite coopercitrus with new api

--- examples/coopercitrus.py
+++ examples/coopercitrus.py
@@ -1,29 +1,29 @@
 import json
-import re
 
-import aiohttp
-
-from robot import CollectorFactory
-from robot import Robot
+from robot import RobotImpl
+from robot.collector.shortcut import *
 
 base_url = 'http://www.coopercitrus.com.br/'
 source = 'Coopercitrus'
 
-cf = CollectorFactory()
-collector = cf.array(
-    '#coluna_esquerda table', cf.obj(
-        cf.remote('a', cf.obj(
-            date=cf.attr('#conteudo h2 + p', regex=re.compile(r'[0-9]+/[0-9]+/[0-9]+')),
-            content=cf.attr('#conteudo p'),
-        )),
-        origin=cf.attr('a', attr='href', prefix=base_url),
-        title=cf.attr('a'),
-        source=cf.const(source)
+collector = array(
+    css('#coluna_esquerda table'),
+    dict(
+        get(
+            pipe(css('a'), attr('href'), any()),
+            dict(
+                date=pipe(css('#conteudo h2 + p'), as_text(), regex(r'[0-9]+/[0-9]+/[0-9]+')),
+                content=pipe(css('#conteudo p'), as_text())
+            )
+        ),
+        origin=pipe(css('a'), attr('href'), any(), url()),
+        title=pipe(css('a'), as_text()),
+        source=const(source),
     )
 )
 
 url_template = 'http://www.coopercitrus.com.br/?pag=noticias&pgN={0}&categoria=&busca='
-robot = Robot(aiohttp, collector)
+robot = RobotImpl()
 for url in [url_template.format(page) for page in range(1, 460)]:
-    result = robot.run(url)
+    result = robot.sync_run(collector, url)
     print(json.dumps(result, indent=4))
