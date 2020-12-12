aca71068b34b4b91df18ecf3f10be39df17ab9da
Ettore Leandro Tognoli <ettoreleandrotognoli@gmail.com>
2020-12-13 00:57:19 +0100

    rewrite dataversity

--- examples/dataversity.py
+++ examples/dataversity.py
@@ -1,20 +1,22 @@
-import aiohttp
+from robot import RobotImpl
+from robot.collector.shortcut import *
 
-from robot import CollectorFactory
-from robot import Robot
+collector = array(
+    css('#primary article'),
+    dict(
+        get(
+            pipe(css('a[href]'), attr('href'), any(), url()),
+            dict(
+                body=pipe(css('.entry-content p'), as_text())
+            )
+        ),
+        title=pipe(css('.entry-title'), as_text()),
+        url=pipe(css('a[href]'), attr('href'), any(), url()),
+    )
+)
 
-cf = CollectorFactory()
-
-collector = cf.array('.listing .post', cf.obj(
-    cf.remote(cf.attr('a[href]', attr='href'), cf.obj(
-            corpo=cf.attr('.entry p')
-    )),
-    titulo=cf.attr('.posttitle'),
-    url=cf.attr('a[href]', attr='href'),
-))
-
-robot = Robot(aiohttp, collector)
-result = robot.run('http://www.dataversity.net/category/education/daily-data/')
+with RobotImpl() as robot:
+    result = robot.sync_run(collector, 'http://www.dataversity.net/category/education/daily-data/')
 
 for r in result:
     print(r)
