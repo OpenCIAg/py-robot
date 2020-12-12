65ff9855a2c0a7ee5142c5e2db27a56f917f7ed6
Ettore Leandro Tognoli <ettoreleandrotognoli@gmail.com>
2020-12-13 00:45:12 +0100

    rewrite iot_eetimes2

--- examples/iot_eetimes2.py
+++ examples/iot_eetimes2.py
@@ -1,20 +1,25 @@
-import aiohttp
-
 from robot import CollectorFactory
-from robot import Robot
+from robot import RobotImpl
+from robot.collector.shortcut import *
 
 cf = CollectorFactory()
 
-collector = cf.array('.theiaStickySidebar ul li', cf.obj(
-    cf.remote(cf.attr('a[href]', attr='href'), cf.obj(
-            corpo=cf.attr('p')
-    )),
-    titulo=cf.attr('span'),
-    url=cf.attr('a[href]', attr='href'),
-))
+collector = array(
+    css('.theiaStickySidebar ul li'),
+    dict(
+        get(
+            pipe(css('a[href]'), attr('href'), any()),
+            dict(
+                body=pipe(css('p'), as_text())
+            )
+        ),
+        title=pipe(css('span'), as_text()),
+        url=pipe(css('a[href]'), attr('href'), any(), url())
+    )
+)
 
-robot = Robot(aiohttp, collector)
-result = robot.run('https://iot.eetimes.com/')
+with RobotImpl() as robot:
+    result = robot.sync_run(collector, 'https://iot.eetimes.com/')
 
 for r in result:
     print(r)
