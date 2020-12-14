779736aaa468726d0a02a074504232398820b6ac
Ettore Leandro Tognoli <ettoreleandrotognoli@gmail.com>
2020-12-14 11:44:46 +0100

    reafactoring: use import alias
    this approach improves IDE inspections

--- src/main/python/robot/collector/shortcut.py
+++ src/main/python/robot/collector/shortcut.py
@@ -1,27 +1,45 @@
-from typing import Any, Dict, Callable
+from typing import Any, Dict
 
-from robot.api import Collector, X, Y
+from robot.api import Collector, X
 from robot.collector import core
+# noinspection PyUnresolvedReferences
+from robot.collector.core import AnyCollector as any
+# noinspection PyUnresolvedReferences
+from robot.collector.core import ArrayCollector as array
+# noinspection PyUnresolvedReferences
+from robot.collector.core import AsTextCollector as as_text
+# noinspection PyUnresolvedReferences
+from robot.collector.core import AsyncFnCollector as afn
+# noinspection PyUnresolvedReferences
+from robot.collector.core import AttrCollector as attr
+# noinspection PyUnresolvedReferences
+from robot.collector.core import ConstCollector as const
+# noinspection PyUnresolvedReferences
+from robot.collector.core import CssCollector as css
+# noinspection PyUnresolvedReferences
+from robot.collector.core import DefaultCollector as default
+# noinspection PyUnresolvedReferences
+from robot.collector.core import DelayCollector as delay
+# noinspection PyUnresolvedReferences
+from robot.collector.core import FilterCollector as filter
+# noinspection PyUnresolvedReferences
+from robot.collector.core import FnCollector as fn
+# noinspection PyUnresolvedReferences
+from robot.collector.core import GetCollector as get
+# noinspection PyUnresolvedReferences
+from robot.collector.core import PipeCollector as pipe
+# noinspection PyUnresolvedReferences
+from robot.collector.core import RegexCollector as regex
+# noinspection PyUnresolvedReferences
+from robot.collector.core import TextCollector as text
+# noinspection PyUnresolvedReferences
+from robot.collector.core import TupleCollector as tuple
+# noinspection PyUnresolvedReferences
+from robot.collector.core import UrlCollector as url
+# noinspection PyUnresolvedReferences
+from robot.collector.core import XPathCollector as xpath
 
-const = core.ConstCollector
-fn = core.FnCollector
 noop = lambda: core.NOOP_COLLECTOR
-afn = core.AsyncFnCollector
-as_text = core.AsTextCollector
-text = core.TextCollector
-attr = core.AttrCollector
-css = core.CssCollector
-xpath = core.XPathCollector
-pipe = core.PipeCollector
-array = core.ArrayCollector
-tuple = core.TupleCollector
-any = core.AnyCollector
-default = core.DefaultCollector
-delay = core.DelayCollector
-filter = core.FilterCollector
-get = core.GetCollector
-url = core.UrlCollector
-regex = core.RegexCollector
 
 
 def dict(*args: Collector[X, Dict[str, Any]], **kwargs: Collector[X, Any]) -> Collector[X, Dict[str, Any]]:
