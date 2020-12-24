a572b458d7117fcc1912543995fe8b47058e4aa7
Ettore Leandro Tognoli <ettoreleandrotognoli@gmail.com>
2020-12-24 14:08:52 +0100

    test against mocked server

--- src/test/python/unit/test_robot.py
+++ src/test/python/unit/test_robot.py
@@ -1,6 +1,62 @@
 from aiounittest import AsyncTestCase
+from pytest_httpserver import HTTPServer
+
 from robot import Robot
 from robot.collector.shortcut import *
+
+main_page = """
+<html>
+   <h1>Hello</h1>
+   <a href="/list/">List</a>
+</html>
+"""
+
+list_page = """
+<html>
+    <body>
+        <ul>
+            <li>
+                <a href="/list/item/1">Link 1</a>
+            </li>
+            <li>
+                <a href="/list/item/2">Link 2</a>
+            </li>
+        </ul>
+    </body>
+</html>
+"""
+
+item_page_template = """
+<html>
+    <body>
+        <article id="main">
+            <p>
+                Item {}
+            </p>
+        </article>
+    </body>
+</html>
+"""
+
+
+def prepare_server(http_server: HTTPServer):
+    http_server.expect_request('/', method='GET').respond_with_data(
+        response_data=main_page,
+    )
+    http_server.expect_request('/list/', method='GET').respond_with_data(
+        response_data=list_page,
+    )
+    for i in range(1, 3):
+        http_server.expect_request(f'/list/item/{i}', method='GET').respond_with_data(
+            status=302,
+            headers={
+                'Location': f'/list-item-{i}'
+            }
+        )
+        http_server.expect_request(f'/list-item-{i}', method='GET').respond_with_data(
+            response_data=item_page_template.format(i),
+        )
+    return http_server
 
 
 class RobotTest(AsyncTestCase):
@@ -18,3 +74,27 @@ class RobotTest(AsyncTestCase):
     async def test_async_run_noop(self):
         result = await self.robot.run(noop())
         self.assertIsNone(result)
+
+    async def test_against_server(self):
+        http_server = HTTPServer()
+        http_server = prepare_server(http_server)
+        with http_server as server:
+            collector = get(
+                const(server.url_for('/')),
+                get(
+                    pipe(css('a[href]'), attr('href'), any(), ),
+                    array(
+                        css('ul > li > a[href]'),
+                        get(
+                            pipe(attr('href'), any()),
+                            pipe(css('#main p'), as_text()),
+                        )
+                    )
+                )
+            )
+            result = await self.robot.run(collector)
+            expected = [
+                "Item 1",
+                "Item 2",
+            ]
+            self.assertEqual(result, expected)
